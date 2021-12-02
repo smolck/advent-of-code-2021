5f0788a0d01300c3ff6cbeda090ab55295fc7581
smolck <46855713+smolck@users.noreply.github.com>
2021-12-01 22:42:19 -0600

    iterators

--- day1/solution-part1.rs
+++ day1/solution-part1.rs
@@ -2,17 +2,28 @@ fn main() {
     // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.filter_map
     let numbers: Vec<i32> = include_str!("input.txt").split('\n').filter_map(|x| x.parse().ok()).collect();
 
-    let mut sum = 0;
-    let mut prev_num = None;
-    for num in numbers {
-        if let Some(prev_num) = prev_num {
-            if num > prev_num {
-                sum += 1;
-            }
-        }
+    let answer = numbers.iter().fold((0, None), |acc, x| {
+        let mut ret = acc;
 
-        prev_num = Some(num);
-    }
+        if let Some(prev) = acc.1 { if x > prev { ret.0 += 1} }
+        ret.1 = Some(x);
 
-    println!("Answer: {}", sum);
+        return ret;
+    }).0;
+
+    // Non-iterator version
+    //
+    // let mut sum = 0;
+    // let mut prev_num = None;
+    // for num in numbers {
+    //     if let Some(prev_num) = prev_num {
+    //         if num > prev_num {
+    //             sum += 1;
+    //         }
+    //     }
+
+    //     prev_num = Some(num);
+    // }
+
+    println!("Answer: {}", answer);
 }
--- day1/solution-part2.rs
+++ day1/solution-part2.rs
@@ -2,23 +2,33 @@ fn main() {
     // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.filter_map
     let numbers: Vec<i32> = include_str!("input.txt").split('\n').filter_map(|x| x.parse().ok()).collect();
 
-    let mut i = 0;
-    let mut sum = 0;
-    let mut prev_num = None;
-    let len = numbers.len();
+    let answer = numbers.windows(3).fold((0, None), |acc: (i32, Option<i32>), x| {
+        let mut ret = acc;
+        let x = x.iter().sum();
 
-    while len - i > 2 {
-        let num = numbers[i] + numbers[i + 1] + numbers[i + 2];
+        if let Some(prev) = acc.1 { if x > prev { ret.0 += 1} }
+        ret.1 = Some(x);
 
-        if let Some(prev_num) = prev_num {
-            if num > prev_num {
-                sum += 1;
-            }
-        }
+        return ret;
+    }).0;
 
-        prev_num = Some(num);
-        i += 1;
-    }
+    // let mut i = 0;
+    // let mut sum = 0;
+    // let mut prev_num = None;
+    // let len = numbers.len();
 
-    println!("Answer: {}", sum);
+    // while len - i > 2 {
+    //     let num = numbers[i] + numbers[i + 1] + numbers[i + 2];
+
+    //     if let Some(prev_num) = prev_num {
+    //         if num > prev_num {
+    //             sum += 1;
+    //         }
+    //     }
+
+    //     prev_num = Some(num);
+    //     i += 1;
+    // }
+
+    println!("Answer: {}", answer);
 }
